9c1fcbf2a2171194a237d92b88069a852976ed41
Kristina0805 <kristya25.95@mail.ru>
2022-12-16 11:10:59 +0500

    change build.gradle

---
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -0,0 +1,10 @@
+package ru.netology.service;
+
+public class CashbackHackService {
+
+    private final int boundary = 1000;   // boundary - граница
+
+    public int remain(int amount) {       // amount - сумма
+        return boundary - amount % boundary;
+    }
+}
