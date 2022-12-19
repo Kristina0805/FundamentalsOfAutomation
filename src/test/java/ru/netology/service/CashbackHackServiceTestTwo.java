f72bbdbde563a99d83045545762c41d918082c64
Kristina0805 <kristya25.95@mail.ru>
2022-12-19 11:33:37 +0500

    new branch and tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTestTwo.java
@@ -0,0 +1,48 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTestTwo {
+
+    @Test
+    public void testRemain() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void testRemainTwo() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void remain() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1480;
+
+        int expected = 520;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+}
